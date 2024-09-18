1a8bb69facd80bada2dd52817a7be29a19761769
lunalin1008 <lunar01011734@gmail.com>
2024-09-18 11:58:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var sidebar = document.getElementById('sidebar');
+    var content = document.querySelector('.content');
+
+    if (sidebar.classList.contains('collapsed')) {
+        content.classList.add('collapsed');
+    } else {
+        content.classList.add('expanded');
+    }
+});
+
 document.getElementById('toggleSidebar').addEventListener('click', function() {
     var sidebar = document.getElementById('sidebar');
     var content = document.querySelector('.content');
     
     sidebar.classList.toggle('collapsed');
     content.classList.toggle('collapsed');
+    content.classList.toggle('expanded');
 });
