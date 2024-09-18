a514577b407901e7153ac4b6603556a508a8456e
lunalin1008 <lunar01011734@gmail.com>
2024-09-18 11:56:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.getElementById('toggleSidebar').addEventListener('click', function() {
     var sidebar = document.getElementById('sidebar');
     var content = document.querySelector('.content');
     
-    console.log('Button clicked!'); // 調試用
     sidebar.classList.toggle('collapsed');
     content.classList.toggle('collapsed');
 });
